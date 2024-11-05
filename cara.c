2333e32d585cfc5cfc09568d26424cffc6633a2c
Arthur Xavier <131879335+zthuzas@users.noreply.github.com>
2024-11-05 20:58:07 -0300

    Update cara.c

--- cara.c
+++ cara.c
@@ -3,11 +3,9 @@
 int main() {
     int N, i, resultado;
 
-    // Loop principal para ler casos de teste até encontrar N = 0
     while (1) {
         scanf("%d", &N);
 
-        // Verifica o fim da entrada
         if (N == 0) {
             break;
         }
@@ -15,7 +13,6 @@ int main() {
         int vitoriasMaria = 0;
         int vitoriasJoao = 0;
 
-        // Lê os resultados dos N jogos e conta as vitórias
         for (i = 0; i < N; i++) {
             scanf("%d", &resultado);
             if (resultado == 0) {
@@ -25,7 +22,6 @@ int main() {
             }
         }
 
-        // Exibe o resultado para o caso de teste atual
         printf("Maria %d Joao %d\n", vitoriasMaria, vitoriasJoao);
     }
 
